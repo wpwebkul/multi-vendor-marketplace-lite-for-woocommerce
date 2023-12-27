f725e17faee49bb38fec0a8b92af6b6dbd8ebd47
dineshyadav.wp <dineshyadav.wp@webkul.in>
2023-12-27 18:47:09 +0530

    Updated pot file

--- multi-vendor-marketplace-lite-for-woocommerce.php
+++ multi-vendor-marketplace-lite-for-woocommerce.php
@@ -51,7 +51,7 @@ defined( 'WKMP_LITE_FILE' ) || define( 'WKMP_LITE_FILE', __FILE__ );
 defined( 'WKMP_LITE_PLUGIN_FILE' ) || define( 'WKMP_LITE_PLUGIN_FILE', plugin_dir_path( __FILE__ ) );
 defined( 'WKMP_LITE_PLUGIN_URL' ) || define( 'WKMP_LITE_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 defined( 'WKMP_LITE_SCRIPT_VERSION' ) || define( 'WKMP_LITE_SCRIPT_VERSION', '1.0.0' );
-defined( 'WKMP_LITE_VERSION' ) || define( 'WKMP_LITE_VERSION', '0.9.3' );
+defined( 'WKMP_LITE_VERSION' ) || define( 'WKMP_LITE_VERSION', '1.0.0' );
 defined( 'WKMP_LITE_DB_VERSION' ) || define( 'WKMP_LITE_DB_VERSION', '5.3.4' );
 defined( 'WKMP_LITE_PLUGIN_BASENAME' ) || define( 'WKMP_LITE_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
 defined( 'WKMP_LITE_WK_CACHING_VERSION' ) || define( 'WKMP_LITE_WK_CACHING_VERSION', '1.0.8' );
